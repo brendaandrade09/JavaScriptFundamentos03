faca25ed8acc6eaf568c8b6d3e4e26c1a810d7db
Brenda <brenda.andrade@edu.senai.br>
2026-08-11 16:43:26 -0300

    Atualização

---
+++ 01-funcoes/01-funcao-simples.js
@@ -0,0 +1,17 @@
+function saudacao(){
+    console.log("Olá, galerinha!!");
+}
+
+saudacao();
+
+function bomDia(){
+console.log("Bom dia!!");
+}
+
+bomDia();
+
+function apresentar(nome){
+    console.log(`Olá, ${nome}!`)
+}
+apresentar("Brenda");
+apresentar("Beren");
