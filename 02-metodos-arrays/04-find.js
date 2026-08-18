f9e8ed0dd1d6e2c2454e07639ae76bfbcb8c8457
Brenda <brenda.andrade@edu.senai.br>
2026-08-18 14:24:59 -0300

    opa

---
+++ 02-metodos-arrays/04-find.js
@@ -0,0 +1,6 @@
+const students = ["Thalis", "Link", "Bella", "Mat"];
+const student = students.find((nome) => nome === "Bella");
+console.log(student);
+
+const student2 = students.find((nome) => nome === "Abc");
+console.log(student2);
---
+++ 02-metodos-arrays/05-find-index.js
@@ -0,0 +1,6 @@
+let names = ["Vic", "Ferdinand", "Isinha", "Benjamino"];
+const indice = names.findIndex((n) => n === "Isinha");
+console.log(indice);
+
+const indice2 = names.findIndex((n) => n === "Abc");
+console.log(indice2);
