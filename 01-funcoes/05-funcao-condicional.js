a325e5f94b879b39ee4711e666a6aba754631e4b
Brenda <brenda.andrade@edu.senai.br>
2026-08-18 11:32:16 -0300

    Atualização 4

---
+++ 01-funcoes/05-funcao-condicional.js
@@ -0,0 +1,8 @@
+function verificaNota(media) {
+    // if (media >= 7) {
+    //     return "Aprovado"
+    // }
+    // return "Reprovado"
+    return media >= 7? "Aprovado" : "Reprovado"
+}
+console.log(verificaNota(7));
---
+++ 01-funcoes/06-function-expression.js
@@ -0,0 +1,8 @@
+function somarTradicional(a, b){
+    return a+b;
+}
+const somarAnonima = function (a, b){
+    return a+b;
+}
+console.log(somarAnonima(5,15));
+console.log(somarTradicional(10,5));
