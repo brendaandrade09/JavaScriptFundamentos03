20161c9ce3dae9940927b194da9837ee5be1b675
Brenda <brenda.andrade@edu.senai.br>
2026-08-18 13:57:35 -0300

    Updaaaaaaaate

--- 02-metodos-arrays/02-map.js
+++ 02-metodos-arrays/02-map.js
@@ -3,5 +3,5 @@ const dobrado = numeros.map((num) => num * 2);
 console.log(dobrado);
 
 let nomes = ["Beren", "Mel", "Lukz", "Jack"];
-let mensagem = nomes.map((nome) => `Olá, ${nome}!`);
+let mensagem = nomes.map((nome) => `Olá, ${nome.toUpperCase()}!`);
 console.log(mensagem);
