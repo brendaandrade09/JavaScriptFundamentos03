5a1ee7d8014596d9335759a7d1f02179117aaa21
Brenda <brenda.andrade@edu.senai.br>
2026-08-18 09:40:11 -0300

    Atualização 3

--- 01-funcoes/02-parametros.js
+++ 01-funcoes/02-parametros.js
@@ -1,9 +1,15 @@
-function apresentar(nome){
-    console.log(`Olá, ${nome}!`);
-}
-apresentar("Beren");
+// function apresentar(nome){
+//     console.log(`Olá, ${nome}!`);
+// }
+// apresentar("Beren");
 
 function somar(a, b){
     console.log(`${a} + ${b} = ${a+b}`);
 }
 somar(5, 8);
+
+function apresentar(nome = "Visitante"){
+    console.log(`Olá, ${nome}!`);
+}
+apresentar();
+apresentar("Beren");
---
+++ 01-funcoes/03-return.js
@@ -0,0 +1,5 @@
+function somar(a, b){
+    return a+b;
+}
+let resultado = somar(10, 15);
+console.log(resultado);
