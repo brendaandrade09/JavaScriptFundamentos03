562f11e4fa8004d73902480137e42d702ae3d544
Brenda <brenda.andrade@edu.senai.br>
2026-08-18 13:51:37 -0300

    Atualizando ae

---
+++ 02-metodos-arrays/02-map.js
@@ -0,0 +1,7 @@
+const numeros = [1, 2, 3, 4, 5];
+const dobrado = numeros.map((num) => num * 2);
+console.log(dobrado);
+
+let nomes = ["Beren", "Mel", "Lukz", "Jack"];
+let mensagem = nomes.map((nome) => `Olá, ${nome}!`);
+console.log(mensagem);
